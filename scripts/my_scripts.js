7f162ea29128fdb8cfe1794b590cb8d478b06166
Raphaeldrf <78796457+Raphaeldrf@users.noreply.github.com>
2022-02-24 19:37:38 +0100

    Challenge 2 invoegen

---
+++ scripts/my_scripts.js
@@ -0,0 +1,31 @@
+function showTime(){
+    var date = new Date();
+    var dname = date.getDay();
+    var mo = date.getMonth();
+    var dnum = date.getDate();
+    var yr = date.getFullYear();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds();
+    
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["dayname", "month", "daynum", "year"];
+    var values = [week[dname], mo, dnum, yr];
+
+    for (var i = 0; i < ids.length; i++)
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+ 
+showTime();
